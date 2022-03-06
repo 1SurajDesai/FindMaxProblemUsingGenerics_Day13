632f5a33205633679f6e61e2795d38ef5f8b2d25
1SurajDesai <surajdesai219@gmail.com>
2022-03-06 15:49:05 +0530

    Updating Program.cs file

--- Program.cs
+++ Program.cs
@@ -10,8 +10,14 @@ namespace FindMaxProblemUsingGenerics
             //obj1.FindMaxNo();
             //FindMaxFrom3Floats obj2 = new FindMaxFrom3Floats();
             //obj2.FindMaxNo();
-            FindMaxFrom3Strings obj3 = new FindMaxFrom3Strings();
-            obj3.FindMaxNo();
+            //FindMaxFrom3Strings obj3 = new FindMaxFrom3Strings();
+            //obj3.FindMaxNo();
+            int[] intArray = { 1, 2, 3, 4 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.printMaxValue();
+            double[] doubleArray = { 2.3, 4.5, 6.7, 23.4 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.printMaxValue();
         }
     }
 }
