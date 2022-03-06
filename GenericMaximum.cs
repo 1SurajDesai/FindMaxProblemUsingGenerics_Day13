e447468ac65f0e74d937468a66b70c0d9151c37f
1SurajDesai <surajdesai219@gmail.com>
2022-03-06 15:47:58 +0530

    Adding refactored code

---
+++ GenericMaximum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxProblemUsingGenerics
+{
+    public class GenericMaximum<T> where T : IComparable
+    {
+        public T[] value;
+
+        public GenericMaximum(T[] value)
+        {
+            this.value = value;
+
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T maxValues(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+        public void printMaxValue()
+        {
+            var max = maxValues(this.value);
+            Console.WriteLine("Maximum value is " + max);
+        }
+    }
+}
