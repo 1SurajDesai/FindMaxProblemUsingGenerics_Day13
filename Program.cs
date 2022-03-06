eb06b9025af37498b92ee4aff88a52e90269ff11
1SurajDesai <surajdesai219@gmail.com>
2022-03-06 12:14:51 +0530

    Adding Program.cs file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace FindMaxProblemUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Problem using Generics");
+            //FindMaxFrom3Integers obj1 = new FindMaxFrom3Integers();
+            //obj1.FindMaxNo();
+            //FindMaxFrom3Floats obj2 = new FindMaxFrom3Floats();
+            //obj2.FindMaxNo();
+            //FindMaxFrom3Strings obj3 = new FindMaxFrom3Strings();
+            //obj3.FindMaxNo();
+        }
+    }
+}
