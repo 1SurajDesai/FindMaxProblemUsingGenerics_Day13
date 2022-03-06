1c1f05323c23e26fee06c55adfcc9b3cdcadca1d
1SurajDesai <surajdesai219@gmail.com>
2022-03-06 12:25:36 +0530

    Adding updated Program.cs file

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@ namespace FindMaxProblemUsingGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Find Maximum Problem using Generics");
+            //Console.WriteLine("Welcome to Find Maximum Problem using Generics");
             //FindMaxFrom3Integers obj1 = new FindMaxFrom3Integers();
             //obj1.FindMaxNo();
             //FindMaxFrom3Floats obj2 = new FindMaxFrom3Floats();
             //obj2.FindMaxNo();
-            //FindMaxFrom3Strings obj3 = new FindMaxFrom3Strings();
-            //obj3.FindMaxNo();
+            FindMaxFrom3Strings obj3 = new FindMaxFrom3Strings();
+            obj3.FindMaxNo();
         }
     }
 }
